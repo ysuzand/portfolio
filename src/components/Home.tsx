2f691cd5dd953e7bcdae9b3ab07eaf6276bfe1a1
Yoko Andrae <yokoandrae@Yokos-MBP.lan>
2024-09-21 18:29:00 +0200

    scroll animation

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,15 @@
-import type { StageContentIndexType } from "./Info";
 import { Canvas } from "@react-three/fiber";
 import { ScrollControls, Scroll } from "@react-three/drei";
 import { Suspense, useState } from "react";
 import Loader from '@/components/Loader'
-import Info from "./Info";
-import Sections from "./Sections";
 import Ball from "@/models/Ball";
+import HomeText from "./HomeText";
 import st from './Home.module.scss'
 
 const Home = () => {
-    const [currentStage, setCurrentStage] = useState<StageContentIndexType>(1);
-    
 
     return (
         <section className={st.wrapper}>
-            <div className={st.element}>
-                {!!currentStage && <Info currentStage={currentStage}/>}
-            </div>
             <Canvas
                 camera={{near: 0.1, far: 1000}}
                 className={st.canvas}
@@ -25,19 +18,18 @@ const Home = () => {
                 <Suspense fallback={<Loader />}>
                     <directionalLight
                         castShadow
-                        // position={[0, 20, -30]}
+                        position={[0, 20, -30]}
                         intensity={1}
                         />
                     <ambientLight visible intensity={2}/>
                     <hemisphereLight intensity={1}/>
                     <spotLight intensity={1} position={[10, 10, 0]}/>
-                    <ScrollControls pages={3} damping={1.2}>
+                    <ScrollControls pages={3} damping={0.25}>
                         <Scroll>
                             <Ball />
-                            <Sections />
                         </Scroll>
                         <Scroll html>
-                            <h1>123</h1>
+                            <HomeText />
                         </Scroll>
                     </ScrollControls>
                 </Suspense>
--- src/models/Ball.tsx
+++ src/models/Ball.tsx
@@ -1,23 +1,72 @@
-import { type Euler, useThree, type Vector3 } from '@react-three/fiber'
-import { useMemo } from 'react';
-const Ball = () => {
+import type { Euler, Vector3 } from '@react-three/fiber'
+import { useEffect, useMemo, useRef } from 'react';
+import { useScroll } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { OrbitControls } from "@react-three/drei";
+import type { BufferGeometry, Mesh, NormalBufferAttributes } from 'three';
 
-    const canvasSize = useThree((state) => state.size)
+const Ball = () => {
+    const ballRef = useRef<Mesh<BufferGeometry<NormalBufferAttributes>>>(null)
+    const data = useScroll();
+    const canvasSize = useThree((state) => state.size);
+
+    const originalZ = useRef(0);
+    const originalX = useRef(0);
+    const prevIndex = useRef(0);
+
+
+    const isTablet = useMemo(() => canvasSize.width >= 768 && canvasSize.width <= 1200, [canvasSize.width]);
+    const isDesktop = useMemo(() => canvasSize.width > 1200, [canvasSize.width]);
+
+
+    useEffect(() => {
+        originalX.current = ballRef.current?.position.x ?? 0;
+        originalZ.current = ballRef.current?.position.z ?? 0;
+    }, [])
+
+    useFrame(() => {
+        // data.offset = current scroll position, between 0 and 1
+        const index = data.offset;
+        
+        if (ballRef?.current) {
+                if (prevIndex.current < index) { // down
+                    console.log('scroll down')
+                 if (ballRef.current.position.z > -200) {
+                    ballRef.current.position.z -= 1;
+                    ballRef.current.position.x += 1;
+                 }
+
+                 prevIndex.current = index;
+
+                } 
+                
+                if (prevIndex.current > index){
+                    console.log('scroll up')
+                    if (ballRef.current.position.z <= originalZ.current) {
+                        ballRef.current.position.z += 1;
+                        ballRef.current.position.x -= 1;
+                    }
+
+                    prevIndex.current = index;
+                    
+                }
+        }
+    })
 
     const ballSize = useMemo(() => {
-        let position: Vector3 = [0,0,-100]
-        let scale = canvasSize.width / 25;
+        let position: Vector3 = [10, -30, -120];
+        let scale = canvasSize.width / 10;
         const rotation: Euler = [-Math.PI / 2, 0, 0]
 
         if (typeof canvasSize === 'object') {
-            if (canvasSize.width < 1000 && canvasSize.width > 769) {
+            if (isTablet) {
                 position = [10, -30, -120]
                 scale = canvasSize.width / 10;
             }
 
-            if (canvasSize.width <= 768) {
-                position = [10, -30, -120]
-                scale = canvasSize.width / 10;
+            if (isDesktop) {
+                position = [0,0,-100];
+                scale = canvasSize.width / 25;
             }
         }
 
@@ -25,10 +74,18 @@ const Ball = () => {
     }, [canvasSize]);
 
     return (
-        <mesh position={ballSize.position} rotation={ballSize.rotation} scale={ballSize.scale}>
+        <>
+        <OrbitControls enableZoom={false} />
+        <mesh
+            ref={ballRef}
+            position={ballSize.position}
+            rotation={ballSize.rotation}
+            scale={ballSize.scale}
+        >
             <sphereGeometry args={[1, 60, 1]} />
             <meshMatcapMaterial color="#ffffff" transparent />
         </mesh>
+        </>
     )
 }
 
